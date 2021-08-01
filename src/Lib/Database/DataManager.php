75e1c9f40f2e4cd30028b911234dc2a4836854c1
Шамиль Прилов <pri@crtweb.ru>
2021-08-01 19:15:28 +0300

    feature-added-runtime-fields-to-datamanager

--- src/Lib/Database/DataManager.php
+++ src/Lib/Database/DataManager.php
@@ -2,33 +2,73 @@
 
 namespace Lib\Database;
 
-class DataManager
+use RuntimeException;
+
+abstract class DataManager
 {
-    private static DecoratorConnection $connection;
+    private static ?\PDO $pdoConnection;
 
-    public static function getList(EntityTable $entity, array $parameters)
+    const DEFAULT_RUNTIME_JOIN_TYPE = 'LEFT';
+
+    private static function getConnection() : \PDO
     {
-        $query = new QueryBuilder($entity);
+        $dbHost = 'mysql';
+        $dbName = 'mydb';
+        $dbUser = 'root';
+        $dbPassword = 'root';
+        $dsn = "mysql:host={$dbHost};dbname={$dbName}";
+        return new \PDO($dsn, $dbUser, $dbPassword);
+    }
 
-        $query->setSelect($parameters['select']);
-        $query->setFilter($parameters['filter']);
+    abstract public static function getTableName() : string;
+
+    abstract public static function getMap() : array;
+
+    /**
+     * @param array $parameters
+     * @return ArResult
+     */
+    public static function getList(array $parameters)
+    {
+        // TODO сделать свои виды исключений для ORM
+
+        $entityClass = static::class;
+        if (!$tableName = $entityClass::getTableName()) {
+            throw new \RuntimeException('Method "getTableName" cant return empty string');
+        }
+
+        $query = new QueryBuilder($tableName);
+
+        $parameters['select'] && $query->setSelect($parameters['select']);
+        $parameters['filter'] && $query->setFilter($parameters['filter']);
+
+        foreach ($parameters['runtime'] as $runtimeTableAlias => $runtimeEntity) {
+            if (!$runtimeEntity['reference']) {
+                throw new \InvalidArgumentException('Missing required field "reference" in runtime');
+            }
+
+            if (!is_array($runtimeEntity['reference'])) {
+                throw new \InvalidArgumentException(sprintf('Field "reference" must have type of array, %s given', gettype($runtimeEntity['reference'])));
+            }
+
+            if (!$runtimeEntity['data_type']) {
+                throw new \InvalidArgumentException('Missing required field "data_type" in runtime');
+            }
+
+            $runtimeEntity['join_type'] = $runtimeEntity['join_type'] ?: self::DEFAULT_RUNTIME_JOIN_TYPE;
+
+            if (!(new $runtimeEntity['data_type']) instanceof DataManager) {
+                throw new RuntimeException('Runtime entity must have parent DataManager');
+            }
+
+            $runtimeTable = $runtimeEntity['data_type']::getTableName();
+            $query->setJoin($runtimeTable, $runtimeTableAlias, $runtimeEntity['join_type'], $runtimeEntity['reference']);
+        }
 
         // TODO вызов событий
 
-        return self::$connection->execQuery($query->getQuery());
-    }
-
-    public static function add(EntityTable $entity, array $fields)
-    {
-    }
-
-    public static function update(EntityTable $entity, array $fields)
-    {
-
-    }
-
-    public static function delete(EntityTable $entityTable, array $rowsIds)
-    {
-
+        echo $query->getQuery();
+        $obFetch = self::getConnection()->query($query->getQuery());
+        return new ArResult($obFetch->fetchAll(\PDO::FETCH_ASSOC));
     }
 }
