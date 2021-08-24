664d27502178c8241852bacebbeb0128be342ad2
Шамиль Прилов <pri@crtweb.ru>
2021-08-24 22:44:57 +0300

    added-sql-type-to-user-entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,30 +8,35 @@ class User extends DataManager
 {
     /**
      * @ORM primary_key ID
+     * @ORM type int
      * @ORM column_name ID
      * @var int|null
      */
     public ?int $id = null;
 
     /**
+     * @ORM type varchar(50)
      * @ORM column_name NAME
      * @var string
      */
     public string $name;
 
     /**
+     * @ORM type varchar(50)
      * @ORM column_name LOGIN
      * @var string
      */
     public string $login;
 
     /**
+     * @ORM type varchar(100)
      * @ORM column_name PASSWORD_HASH
      * @var string
      */
     public string $passwordHash;
 
     /**
+     * @ORM type varchar(100)
      * @ORM column_name PICTURE_URL
      * @var string|null
      */
