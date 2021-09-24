2a9ffc474bb1ff0d89f669a722eeb4c106cb68f6
Шамиль Прилов <pri@crtweb.ru>
2021-09-24 23:55:50 +0300

    избавился от заглушек в классе коллекций

--- src/Lib/Database/Collection/LazyCollection.php
+++ src/Lib/Database/Collection/LazyCollection.php
@@ -3,13 +3,9 @@
 namespace Lib\Database\Collection;
 
 use IteratorAggregate;
-use Lib\Database\Column\IntegerColumn;
 use Lib\Database\Hydrator\Hydrator;
 use Lib\Database\MetaData\MetaDataEntity;
-use Lib\Database\Property\Property;
-use Lib\Database\PropertyMap\PropertyMap;
 use Lib\Database\Query\QueryBuilder;
-use Lib\Database\Reader\EntityReader;
 use Lib\Database\Relations\BaseRelation;
 
 class LazyCollection implements IteratorAggregate
@@ -31,36 +27,11 @@ class LazyCollection implements IteratorAggregate
             $this->relation->getTargetColumn()
         );
 
-
-        // Данные полученные якобы от БД
-        $arMock = [
-            [
-                'MIME_TYPE' => 'image/jpeg',
-                'PATH' => '/var/www/bitrix/12.jpg',
-                'FILE_ID' => 123,
-                'EXTENSION' => 'jpeg'
-            ],
-            [
-                'MIME_TYPE' => 'image/jpg',
-                'PATH' => '/var/www/bitrix/995.jpg',
-                'FILE_ID' => 124,
-                'EXTENSION' => 'png'
-            ]
-        ];
-
-        // Структура которую мы получили из ридера сущностей
-        $reader = (new EntityReader())
-            ->setEntityMapping([
-                new PropertyMap(new Property('path'), new IntegerColumn('PATH')),
-                new PropertyMap(new Property('mime_type'), new IntegerColumn('MIME_TYPE')),
-                new PropertyMap(new Property('file_id'), new IntegerColumn('FILE_ID')),
-                new PropertyMap(new Property('extension'), new IntegerColumn('EXTENSION'))
-            ]);
-
+        $dataCollection = (new QueryBuilder())->exec();
         $arIterable = [];
 
-        foreach ($arMock as $mock) {
-            $arIterable[] = Hydrator::getEntity(new MetaDataEntity($this->relation->getTargetClassName(), $reader), new QueryBuilder($mock));
+        foreach ($dataCollection as $item) {
+            $arIterable[] = Hydrator::getEntity(new MetaDataEntity($this->relation->getTargetClassName()), $item);
         }
 
         return new \ArrayIterator($arIterable);
