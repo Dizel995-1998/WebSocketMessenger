d5cafe6222f7f964e6a31f80b3007fd12484e070
Шамиль Прилов <shomaman123@gmail.com>
2021-10-15 21:53:56 +0300

    выключил лог warning сообщений

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
  * Точка входа в приложение
  */
 
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ERROR | E_PARSE);
 ini_set('xdebug.var_display_max_depth', 10);
 ini_set('xdebug.var_display_max_children', 256);
 ini_set('xdebug.var_display_max_data', 1024);
