f5cacec9151d0b561e10db317fd87501257ac010
Шамиль Прилов <shomaman123@gmail.com>
2021-10-16 20:55:59 +0300

    Починил гидратор

--- src/Lib/Database/Hydrator/Hydrator.php
+++ src/Lib/Database/Hydrator/Hydrator.php
@@ -5,6 +5,7 @@ namespace Lib\Database\Hydrator;
 use Lib\Database\Collection\LazyCollection;
 use Lib\Database\Reader\IReader;
 use ReflectionClass;
+use RuntimeException;
 
 class Hydrator
 {
@@ -13,20 +14,19 @@ class Hydrator
         $reflectionClass = new ReflectionClass($metaData->getEntityName());
         $ormEntity = $reflectionClass->newInstanceWithoutConstructor();
 
-        foreach ($metaData->getProperties() as $propertyName => $columnName) {
+        foreach ($metaData->getProperties() as $propertyName => $column) {
             $propertyReflector = $reflectionClass->getProperty($propertyName);
 
             /** Если свойство не nullable типа, а в БД нет для него данных */
             if ($propertyReflector->getType()
                 && !$propertyReflector->getType()->allowsNull()
-                && empty($dbData[$columnName->getName()])
+                && empty($dbData[$column->getName()])
             ) {
-                continue;
-                throw new \RuntimeException(sprintf('Trying write null to not nullable property "%s", entity "%s"', $propertyName, $metaData->getEntityName()));
+                throw new RuntimeException(sprintf('Trying write null to not nullable property "%s", entity "%s"', $propertyName, $metaData->getEntityName()));
             }
 
             $propertyReflector->setAccessible(true);
-            $propertyReflector->setValue($ormEntity, $dbData[$columnName->getName()] ?? null);
+            $propertyReflector->setValue($ormEntity, $dbData[$column->getName()] ?? null);
         }
 
         if ($associations = $metaData->getRelations()) {
